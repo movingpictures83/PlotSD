d67ec162308b5e9cb470fc608fee6bd675431cc6
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-11-03 14:20:51 -0500

    Initial

---
+++ PlotSDPlugin.R
@@ -0,0 +1,22 @@
+## ----para, echo = FALSE, results='hide'---------------------------------------
+BiocStyle::markdown()
+knitr::opts_chunk$set(dev="png",fig.show="hold",
+               fig.width=4,fig.height=4.5,fig.align="center",
+               message=FALSE,collapse=TRUE)
+
+## ----library------------------------------------------------------------------
+library(rnaseqcomp)
+
+input <- function(inputfile) {
+   myDat <<- readRDS(inputfile)
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+pdf(outputfile)
+## ----sd-----------------------------------------------------------------------
+plotSD(myDat,ylim=c(0,1.4))
+
+
+}
